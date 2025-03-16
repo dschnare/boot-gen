9980d4caf0c8f4546e02cfc1d6e6f7bbe688f8a1
Darren Schnare <github@darrenschnare.com>
2025-03-16 13:15:33 -0400

    Add extract_markdown.py that has functions to extract images and links from markdown

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,14 @@
+import unittest
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+class TestExtractMarkdown(unittest.TestCase):
+  def test_should_extract_markdown_images(self):
+    self.assertEqual(extract_markdown_images("some text ![alt text](image.png) other text ![alt text](image2.png)"), [("alt text", "image.png"), ("alt text", "image2.png")])
+    self.assertEqual(extract_markdown_images("some text other text"), [])
+
+  def test_should_extract_markdown_links(self):
+    self.assertEqual(extract_markdown_links("some text [link](https://example.com) other text ![alt text](https://example2.com)"), [("link", "https://example.com")])
+    self.assertEqual(extract_markdown_links("some text other text"), [])
+
+if __name__ == "__main__":
+  unittest.main()
